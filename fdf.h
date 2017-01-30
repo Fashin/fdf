498aceceb4be0bf7d6bec4d2187c440de219eb74
Cyprian BEAUVOIS <cbeauvoi@e1r11p16.42.fr>
2017-01-30 12:22:35 +0100

    add key binding to exit the windows

--- fdf.h
+++ fdf.h
@@ -6,11 +6,19 @@
 /*   By: cbeauvoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/30 10:59:10 by cbeauvoi          #+#    #+#             */
-/*   Updated: 2017/01/30 11:01:03 by cbeauvoi         ###   ########.fr       */
+/*   Updated: 2017/01/30 12:22:09 by cbeauvoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 #include "minilibx/mlx.h"
+#include <stdlib.h>
+
+/*
+ *Ne pas oublier d'enlever ces fonctions avant de push
+ *
+ * */
+#include <stdio.h>
+/*****************************************************/
 #endif
